01260ac7ce698e80f1c0abf2920b52b7dc2a2996
inker <anton.veselyev@gmail.com>
2017-11-30 00:34:39 +0300

    removed unnecessary loaders & plugins

--- webpack/rules.js
+++ webpack/rules.js
@@ -1,19 +1,10 @@
-module.exports = env => [
+module.exports = (env) => [
   {
     test: /\.js$/,
     loader: 'babel-loader',
     exclude: /node_modules/,
     options: {
       cacheDirectory: true,
-    },
-  },
-  {
-    test: /\.(png|jpg|jpeg|gif|svg)$/,
-    use: {
-      loader: 'url-loader',
-      options: {
-        limit: 1,
-      },
     },
   },
 ].filter(item => item)
